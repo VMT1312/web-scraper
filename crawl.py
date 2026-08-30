72fbdcc0d9248afe9c3d86b370fcb1548af12cdc
VMT1312 <thiemmavinh123@gmail.com>
2026-08-30 10:52:16 +0800

    implement functions to extract URLs and images from HTML

--- crawl.py
+++ crawl.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlsplit
+from urllib.parse import urlsplit, urljoin
 from bs4 import BeautifulSoup, Tag
 
 
@@ -26,3 +26,33 @@ def get_first_paragraph_from_html(html: str) -> str:
     else:
         first_p = soup.find("p")
     return first_p.get_text(strip=True) if isinstance(first_p, Tag) else ""
+
+
+def get_urls_from_html(html, base_url: str) -> list[str]:
+    urls = []
+
+    soup = BeautifulSoup(html, "html.parser")
+    a_tags = soup.find_all("a")
+    for tag in a_tags:
+        if not isinstance(tag, Tag):
+            continue
+        url = tag.get("href")
+        if isinstance(url, str) and url:
+            urls.append(urljoin(base_url, url))
+
+    return urls
+
+
+def get_images_from_html(html, base_url: str) -> list[str]:
+    urls = []
+
+    soup = BeautifulSoup(html, "html.parser")
+    img_tags = soup.find_all("img")
+    for tag in img_tags:
+        if not isinstance(tag, Tag):
+            continue
+        path = tag.get("src")
+        if isinstance(path, str) and path:
+            urls.append(urljoin(base_url, path))
+
+    return urls
--- test_crawl.py
+++ test_crawl.py
@@ -3,6 +3,8 @@ from crawl import (
     normalize_url,
     get_heading_from_html,
     get_first_paragraph_from_html,
+    get_urls_from_html,
+    get_images_from_html,
 )
 
 
@@ -113,6 +115,66 @@ class TestCrawl(unittest.TestCase):
 </html>"""
         actual = get_first_paragraph_from_html(input_body)
         expected = ""
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com"]
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = (
+            '<html><body><a href="/doc.html"><span>Boot.dev</span></a></body></html>'
+        )
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/doc.html"]
+        self.assertEqual(actual, expected)
+
+    def test_get_urls_err(self):
+        input_url = "https://crawler-test.com"
+        input_body = """
+<html>
+    <body>
+        <a href="/valid-path">Valid Link</a>
+        <a>No href attribute here</a>
+        <a href="">Empty href</a>
+    </body>
+</html>
+"""
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/valid-path"]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_relative(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><img src="https://crawler-test.com/logo.png" alt="Logo"></body></html>'
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
+        self.assertEqual(actual, expected)
+
+    def test_get_images_err(self):
+        input_url = "https://crawler-test.com"
+        input_body = """
+<html>
+    <body>
+        <img src="/logo.png" alt="Valid image">
+        <img alt="No src attribute here">
+        <img src="" alt="Empty src">
+    </body>
+</html>
+"""
+        actual = get_images_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com/logo.png"]
         self.assertEqual(actual, expected)
 
 
